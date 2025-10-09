a3c18fb15f569568d45644230d486205c09d74b8
Christopher Dale <cdale_red@yahoo.com>
2025-10-09 13:59:24 -0400

    Docker updates

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
-if [ -f package.json ]; then
+# If Node.js is needed, install based on .nvmrc if present
+if [ -f .nvmrc ]; then
+  bash -i -c "nvm install && nvm use"
+elif [ -f package.json ]; then
   bash -i -c "nvm install --lts && nvm install-latest-npm"
-  npm i
-  npm run build
 fi
+
+# Install npm dependencies if package.json exists
+if [ -f package.json ]; then
+  npm ci || npm install
+  npm run build || true
+fi
+
+# Ensure Ruby gems are installed
+bundle check || bundle install
 
 # Install dependencies for shfmt extension
 curl -sS https://webi.sh/shfmt | sh &>/dev/null
@@ -14,5 +25,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggestions\3/" ~/.zshrc
 
-# Avoid git log use less
+# Avoid git log using less
 echo -e "\nunset LESS" >>~/.zshrc
